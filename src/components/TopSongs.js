b3d0665fffba5b571d777d3e874567fdf5fd46ca
jmm <josemmazzei@gmail.com>
2020-02-10 18:12:39 -0300

    WIP for the TopSongs.js component

--- src/components/TopSongs.js
+++ src/components/TopSongs.js
@@ -1,88 +1,92 @@
 import React, { Component } from 'react'
 
 export class TopSongs extends Component {
-<<<<<<< HEAD
     state = {
-        inputArtist: 'cher',
-        topSongs: ''
+        fetchedData: '',
+        tab: 'songs'
     }
 
-    componentDidMount(){
-        fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            this.setState({topSongs: res.toptracks.track})
-        })
+    componentDidMount() {
+        let required = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
+        this._incialFetchData(required)
     }
 
-=======
->>>>>>> 6f8c3ddc268e1f393f120c9fb8f446eeff00d6e0
+    _incialFetchData = async (required) => {
+        await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.${required}&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                this.state.tab === 'songs'
+                    ? this.setState({ fetchedData: res.toptracks.track })
+                    : this.setState({ fetchedData: res.topalbums.album })
+            })
+
+        sessionStorage.setItem('songsData', JSON.stringify(this.state.fetchedData))
+    }
+
+    componentWillUpdate(){
+        console.log('will: '+ this.state.tab);
+    }
+
+    _handleClick = (e) => {
+        console.log(e.target)
+        this.setState({ tab: this.state.tab === 'songs' ? 'albums' : 'songs' })
+        let required = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
+        console.log('sety: ' + this.state.tab)
+
+        if (this.state.tab === 'songs') {
+            this.setState({ fetchedData: JSON.parse(sessionStorage.songsData) })
+        } else {
+            if (!sessionStorage.albumsData) {
+                (async ()=> {
+                    await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.${required}&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
+                        .then(res => res.json())
+                        .then(res => {
+    
+                            this.state.tab === 'songs'
+                                ? this.setState({ fetchedData: res.topalbums.album })
+                                : this.setState({ fetchedData: res.toptracks.track })
+    
+                        })
+                        sessionStorage.setItem('albumsData', JSON.stringify(this.state.fetchedData))
+                })()
+
+            } else {
+                this.setState({ fetchedData: JSON.parse(sessionStorage.albumsData) })
+            }
+        }
+    }
+
     render() {
+        console.log('render: '+this.state.tab)
+
         return (
             <nav className="panel">
                 <p className="panel-heading">
-                    Top Songs
+                    {this.state.tab === 'songs' ? 'Top Songs' : 'Top Albums'}
                 </p>
                 <p className="panel-tabs">
-                    <a className="is-active">Top Songs</a>
-                    <a>Top Albums</a>
+                    <a onClick={this._handleClick}>
+                        {this.state.tab === 'songs' ? 'Top Albums' : 'Top Songs'}
+                    </a>
                 </p>
-<<<<<<< HEAD
 
                 {
-                    typeof this.state.topSongs === "string"
-                    ? null
-                    : this.state.topSongs.map((e,i) => {
-                        return i < 5 
-                        ?
-                        <a key={e.name} className="panel-block is-active" href={`/${e.name}`}>
-                            <span className="panel-icon">
-                                <i className="fas fa-book" aria-hidden="true"></i>
-                            </span>
-                            {e.name}
-                        </a>
-                        : null
-                    })
+                    typeof this.state.fetchedData === "string"
+                        ? null
+                        : this.state.fetchedData.map((e, i) => {
+                            return i < 5
+                                ?
+                                <a key={e.name} className="panel-block is-active" href={`/${e.name}`}>
+                                    <span className="panel-icon">
+                                        <i className="fas fa-book" aria-hidden="true"></i>
+                                    </span>
+                                    {e.name}
+                                </a>
+                                : null
+                        })
                 }
-=======
-                <a className="panel-block is-active">
-                    <span className="panel-icon">
-                        <i className="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    Song n°1
-                </a>
-                <a className="panel-block">
-                    <span className="panel-icon">
-                        <i className="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    Song n°2
-                </a>
-                <a className="panel-block">
-                    <span className="panel-icon">
-                        <i className="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    Song n°3
-                </a>
-                <a className="panel-block">
-                    <span className="panel-icon">
-                        <i className="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    Song n°4
-                </a>
-                <a className="panel-block">
-                    <span className="panel-icon">
-                        <i className="fas fa-code-branch" aria-hidden="true"></i>
-                    </span>
-                    Song n°5
-                </a>
-                <a className="panel-block">
-                    <span className="panel-icon">
-                        <i className="fas fa-code-branch" aria-hidden="true"></i>
-                    </span>
-                    Song n°6
-                </a>
->>>>>>> 6f8c3ddc268e1f393f120c9fb8f446eeff00d6e0
+
             </nav>
         )
     }
