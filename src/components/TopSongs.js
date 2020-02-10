6a512b54ffb27aba22a62219a97dca44f84eafcd
jmm <josemmazzei@gmail.com>
2020-02-10 19:00:26 -0300

    Refactor _fetchData and delete _incialFetchData

--- src/components/TopSongs.js
+++ src/components/TopSongs.js
@@ -7,64 +7,40 @@ export class TopSongs extends Component {
     }
 
     componentDidMount() {
-        let required = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
-        this._incialFetchData(required).then(res => 
+        let reqParam = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
+        this._fetchData(reqParam).then(res => 
             sessionStorage.setItem('songsData', JSON.stringify(res)))
     }
 
-    _incialFetchData = async (required) => {
+    _fetchData = async (reqParam) => {
         let responseAPI = {}
-        await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.${required}&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
+        await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.${reqParam}&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
             .then(res => res.json())
             .then(res => {
-                if (this.state.tab === 'songs') {
+                if (res.toptracks) {
                     responseAPI = res.toptracks.track
                     this.setState({ fetchedData: res.toptracks.track })
                 } else {
                     responseAPI = res.topalbums.album
                     this.setState({ fetchedData: res.topalbums.album })
-                }
+                } 
             })
 
         return await new Promise((resolve, reject) => resolve(responseAPI));
-    }
-
-    _fetchData = async (required) => {
-        let responseAPI = {}
-        await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.${required}&artist=${this.props.artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
-        .then(res => res.json())
-        .then(res => {
-                if (this.state.tab === 'songs') {
-                    responseAPI = res.topalbums.album
-                    this.setState({ fetchedData: res.topalbums.album })
-                } else {
-                    responseAPI = res.toptracks.track
-                    this.setState({ fetchedData: res.toptracks.track })
-                }
-            })
-
-        return await new Promise((resolve, reject) => resolve(responseAPI));
-    }
-
-    componentWillUpdate(){
-        console.log('will: '+ this.state.tab);
-    }
-    componentDidUpdate(){
-        console.log('did: '+ this.state.tab);
     }
 
 
     _handleClick = (e) => {
         console.log(e.target)
         this.setState({ tab: this.state.tab === 'songs' ? 'albums' : 'songs' })
-        let required = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
+        let reqParam = this.state.tab === 'songs' ? 'gettoptracks' : 'gettopalbums'
         console.log('sety: ' + this.state.tab)
 
         if (this.state.tab === 'songs') {
             this.setState({ fetchedData: JSON.parse(sessionStorage.songsData) })
         } else {
             if (!sessionStorage.albumsData) {
-                this._fetchData(required).then( res => 
+                this._fetchData(reqParam).then( res => 
                     sessionStorage.setItem('albumsData', JSON.stringify(res))
                 )
             } else {
@@ -86,7 +62,6 @@ export class TopSongs extends Component {
                         {this.state.tab === 'songs' ? 'Top Albums' : 'Top Songs'}
                     </a>
                 </p>
-
                 {
                     typeof this.state.fetchedData === "string"
                         ? null
@@ -102,7 +77,6 @@ export class TopSongs extends Component {
                                 : null
                         })
                 }
-
             </nav>
         )
     }
