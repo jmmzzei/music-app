22de7519fd79aa0cc8e003a7e7a579f2b8440c47
jmm <josemmazzei@gmail.com>
2020-02-13 17:20:22 -0300

    Add DetailContainer component to clean the layout

---
+++ src/components/DetailContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { List } from '../components/List'
+
+export class DetailContainer extends Component {
+    render() {
+        return (
+            <div className="columns">
+                <div className="column">
+                    <img
+                        src={this.props.img}
+                        className="is-block is-centered image"
+                        alt="artist"
+                    />
+                    <div className="box">
+                        <p className="title">LISTENERS: {this.props.listeners}</p>
+                    </div>
+                    <List 
+                        iterable={this.props.similar}
+                        title="RELATED ARTISTS"
+                    />
+                </div>
+
+                <div className="column is-half">
+                    <div className="box">
+                        <p className="title">BIO</p>
+                        <p className="has-text-left">{this.props.bio}</p>
+                    </div>
+                </div>
+                
+                <div className="column">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
