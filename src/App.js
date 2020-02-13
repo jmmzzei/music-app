f1a357de166e5e3848a673667281a0b988aa27ea
jmm <josemmazzei@gmail.com>
2020-02-13 17:33:11 -0300

    Add capability to childrens to set state in the App component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,61 @@ import {Switch, Route} from 'react-router-dom'
 import 'bulma/css/bulma.css'
 import './App.css'
 import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
-import { NotFound } from "./pages/NotFound";
+import { Artist } from './pages/Artist'
+import { NotFound } from "./pages/NotFound"
+import { Song } from './pages/Songs'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      resultado: {},
+      song: '',
+      artistName: ''
+    }
+  }
+
+  
+  _handleResults = results => {
+    console.log(results)
+    
+    this.setState({ artistName: results.results.name })
+    this.setState({ resultado: results })
+    console.log(this.state.artistName)
+  }
+  
+  _handleSingleSong = (song) => {
+    console.log(song)
+    this.setState({song: song})
+  }
+
+  _handleCallback = (artist) => {
+    // this.setState({})
+    console.log(artist);
+  }
+
   render() {
+    console.log(this.state.artistName)
     return (
+      
       <div className="App">
-        <Switch>
-        <Route exact path='/' component={Home} /> 
-        <Route path='/detail/:id' component={Detail} />
-        <Route component={NotFound}/>
+
+          <Switch>
+          <Route exact path='/' render={() => <Home 
+          onResults={this._handleResults}
+          onCallback={this._handleSingleSong} /> } /> 
+          <Route path='/artist/:id' render={() => <Artist {...this.state}/>} 
+          />
+          <Route path='/song/:artist/:song' component={router => <Song 
+              router={router}
+              onCallback={this._handleCallback}
+              />
+              }
+              />
+          <Route component={NotFound}/>
         </Switch>
+
+
       </div>
     )
   }
