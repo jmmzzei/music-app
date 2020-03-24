e6cfe36a92a92fce29deffde7cd3cde7bd15dd8f
jmm <josemmazzei@gmail.com>
2020-03-24 13:18:59 -0300

    Add columns layout in Song.js

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -16,7 +16,7 @@ export class Song extends Component {
 		let responseAPI = {}
 		await fetch(
 			`https://ws.audioscrobbler.com/2.0/?method=track.${reqParam}&artist=${this.props.match.params.artist.toLowerCase()}&track=${this.props.match.params.song.toLowerCase()}&api_key=${
-				process.env.REACT_APP_API_KEY
+			process.env.REACT_APP_API_KEY
 			}&format=json`
 		)
 			.then(res => res.json())
@@ -36,14 +36,14 @@ export class Song extends Component {
 			})
 	}
 
-    componentDidUpdate(previousProps) {
-        const currentSearch = this.props.location.pathname
-        const previousSearch = previousProps.location.pathname
-        if (currentSearch !== previousSearch) {
-            this._fetchData("getsimilar")
-            this._fetchData("getinfo")
-        }
-      }
+	componentDidUpdate(previousProps) {
+		const currentSearch = this.props.location.pathname
+		const previousSearch = previousProps.location.pathname
+		if (currentSearch !== previousSearch) {
+			this._fetchData("getsimilar")
+			this._fetchData("getinfo")
+		}
+	}
 
 	componentDidMount() {
 		this._fetchData("getsimilar")
@@ -55,25 +55,29 @@ export class Song extends Component {
 			<>
 				<Navbar hasButton />
 				{Object.keys(this.state.similar).length === 0 &&
-				this.state.similar.constructor === Object ? null : (
-					<div>
-						<h1 className="title">{this.state.info.name}</h1>
-						<Level
-							artist={this.state.info.artist.name}
-							album={this.state.info.album.title}
-							duration={this.state.info.duration}
-							listeners={this.state.info.listeners}
-						/>
-					</div>
-				)}
+					this.state.similar.constructor === Object ? null : (
+						<div>
+							<h1 className="title">{this.state.info.name}</h1>
+							<Level
+								artist={this.state.info.artist.name}
+								album={this.state.info.album.title}
+								duration={this.state.info.duration}
+								listeners={this.state.info.listeners}
+							/>
+						</div>
+					)}
 				{Object.keys(this.state.similar).length === 0 &&
-				this.state.similar.constructor === Object ? null : (
-					<List
-						title="SIMILAR TRACKS"
-						iterable={this.state.similar}
-						songList
-					/>
-				)}
+					this.state.similar.constructor === Object ? null : (
+						<div className="columns">
+							<div className="column is-half is-offset-one-quarter">
+								<List
+									title="SIMILAR TRACKS"
+									iterable={this.state.similar}
+									songList
+								/>
+							</div>
+						</div>
+					)}
 			</>
 		)
 	}
