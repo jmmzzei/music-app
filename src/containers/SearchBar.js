672bae9f8d7d13dea6fdf31d4d21b8bc927b3cdc
jmm <josemmazzei@gmail.com>
2020-07-13 15:22:48 -0300

    Store previous searches in the session storage

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,34 +1,40 @@
-import React, { Component } from "react"
-import { SearchBarStyled } from "../components/SearchBarStyled"
+import React, {Component} from 'react'
+import {SearchBarStyled} from '../components/SearchBarStyled'
 import {fetchArtistData} from '../helpers/fetchArtistData'
 
 export class SearchBar extends Component {
   state = {
-    inputArtist: "",
+    inputArtist: '',
     loading: false,
   }
 
   _handleChange = e => {
-    this.setState({ inputArtist: e.target.value.toUpperCase() })
+    this.setState({inputArtist: e.target.value.toUpperCase()})
   }
 
   _handleSubmit = async e => {
     e.preventDefault()
-    if (this.state.inputArtist) {
-      sessionStorage && sessionStorage.clear()
+    if (this.state.inputArtist !== 0) {
+      this.setState({loading: true})
 
-      sessionStorage.setItem("input", this.state.inputArtist)
-      this.setState({ loading: true })
+      let searchedArtists = sessionStorage.getItem('artistData')
+        ? JSON.parse(sessionStorage.getItem('artistData'))
+        : {}
 
-      if (this.state.inputArtist.length !== 0) {
-        let response = await fetchArtistData("getinfo", this.state.inputArtist)  
-        if(response.success) {
+      if (searchedArtists[this.state.inputArtist]){
+        this.props.onResults(searchedArtists[this.state.inputArtist])
+
+      } else {
+        let response = await fetchArtistData('getinfo', this.state.inputArtist)
+        if (response.success) {
           this.props.onResults(response.artist)
+          searchedArtists[this.state.inputArtist] = response.artist
+          sessionStorage.setItem('artistData', JSON.stringify(searchedArtists))
         } else {
           this.props.onResults({})
         }
-        this.setState({ inputArtist: "", loading: false })
       }
+      this.setState({inputArtist: '', loading: false})
     }
   }
 
