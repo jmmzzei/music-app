719dc64889c8f02fe5225eacee346d323e3cfafb
José <josemmazzei@gmail.com>
2020-08-06 15:45:57 -0300

    Delete package from lambda folder

--- src/functions/fetch.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-
-exports.handler = function (event, context, callback) {
-  console.log(0)
-    callback(null, { 
-       statusCode: 200, 
-        body: "Hello World" 
-    });
-}
