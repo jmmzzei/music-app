dff782f3041a8675e81f17c59c805e805026583e
José <josemmazzei@gmail.com>
2020-08-06 16:16:35 -0300

    Update test function

--- lambda/test/test.js
+++ lambda/test/test.js
@@ -1,9 +1,8 @@
 const fetch = require('node-fetch')
 
 exports.handler = async function(event, context) {
-  console.log('test')
   try {
-    const response = await fetch('https://icanhazdadjoke.com', {
+    const response = await fetch(process.env.URLA, {
       headers: { Accept: 'application/json' },
     })
     if (!response.ok) {
@@ -11,10 +10,10 @@ exports.handler = async function(event, context) {
       return { statusCode: response.status, body: response.statusText }
     }
     const data = await response.json()
-
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke }, {urla : process.env.URLA}),
+      body: JSON.stringify({ msg: data.joke}),
     }
   } catch (err) {
     console.log(err) // output to netlify function log
